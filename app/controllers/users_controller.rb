53e7d71375c3ba8b5a5c40f69880ace2bc8e138c
Your Name <you@example.com>
2017-10-19 13:44:26 +0300

    action edit and update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_action :signed_in_user, only: [:edit, :update]
+  before_action :correct_user,   only: [:edit, :update
+
   def show
     @user = User.find(params[:id])
   end
@@ -6,6 +9,7 @@ class UsersController < ApplicationController
   def new
      @user = User.new
   end
+
   def create
     @user = User.new(user_params)
     if @user.save
@@ -16,10 +20,35 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+    def update
+      if @user.update_attributes(user_params)
+        flash[:success] = "Profile updated"
+        redirect_to @user
+      else
+        render 'edit'
+      end
+    end
+
     private
 
-      def user_params
-        params.require(:user).permit(:name, :email, :password,
+  def user_params
+    params.require(:user).permit(:name, :email, :password,
                                      :password_confirmation)
-      end
+  end
+
+  def signed_in_user
+    unless signed_in?
+      store_location
+      redirect_to signin_url, notice: "Please sign in."
+    end
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless current_user?(@user)
+  end
 end
