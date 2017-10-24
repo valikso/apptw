0d0e78101568aa46937d8a30476f5222c5df0a72
Your Name <you@example.com>
2017-10-24 16:02:40 +0300

    add partials actions create destroy + micropost feed

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'microposts/create'
+
+  get 'microposts/destroy'
+
   get 'users/show'
   get 'users/new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
